3767db5eb241bc0956a61b06cc2f8359a64cccce
Nima Mashhadi M. Reza <49960770+n25a@users.noreply.github.com>
2022-11-20 01:00:26 +0330

    🗄️ Add database package (#2)
    * add the main interface of the database
    
    * add timescaledb to databases

---
+++ internal/database/database.go
@@ -0,0 +1,12 @@
+package database
+
+// Database is the interface that wraps the basic database operations.
+type Database interface {
+	Connect() error
+	CreateTable() error
+	Close() error
+	Insert(query string, data interface{}) error
+}
+
+// MapSubjectToQuery is a map that contains the subject as key and the query as value.
+type MapSubjectToQuery map[string]string
