93ad8a13c59e4680072111c017847d7c477248e0
Nima Mashhadi <nima.mashhadi@snapp.cab>
2022-11-21 11:00:06 +0330

    update interface

--- internal/mq/message_queue.go
+++ internal/mq/message_queue.go
@@ -1,14 +1,24 @@
 package mq
 
-type MessageQueue interface {
-	Connect() error
-	Close() error
-	Publish(subject string, data interface{}) error
-	Subscribe(subject string, handler func(payload interface{})) error
-}
+import "github.com/nats-io/nats.go"
 
 type MQType string
 
 const (
 	NATS MQType = "nats"
 )
+
+type MessageQueue interface {
+	Connect() error
+	Close() error
+	Publish(subject string, data interface{}) error
+	Subscribe(subject string, payload interface{}, handler func(payload interface{}) nats.MsgHandler) error
+	UnSubscribe() error
+}
+
+func NewMessageQueue(mqType MQType) MessageQueue {
+	if mqType == NATS {
+		return NewNatsMQ()
+	}
+	return nil
+}
