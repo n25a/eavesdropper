9bf27a8c77b3b9d04a65c9205250461a4099cb37
Nima Mashhadi <nima.mashhadi@snapp.cab>
2022-11-21 19:12:02 +0330

    add Insert function

--- internal/database/timescaledb.go
+++ internal/database/timescaledb.go
@@ -1,6 +1,16 @@
 package database
 
-type timescaleDB struct{}
+import (
+	"context"
+
+	"github.com/n25a/eavesdropper/internal/config"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type timescaleDB struct {
+	dbPool *pgxpool.Pool
+}
 
 // NewTimescaleDB returns a new instance of timescaleDB.
 func NewTimescaleDB() Database {
@@ -9,7 +19,13 @@ func NewTimescaleDB() Database {
 
 // Connect connects to the database.
 func (t *timescaleDB) Connect() error {
-	panic("Not implemented")
+	var err error
+	ctx := context.Background()
+	t.dbPool, err = pgxpool.Connect(ctx, config.C.Database.Conf.Address)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // CreateTable creates all tables in the database.
@@ -19,10 +35,15 @@ func (t *timescaleDB) CreateTable() error {
 
 // Close closes the connection with the database.
 func (t *timescaleDB) Close() error {
-	panic("Not implemented")
+	t.dbPool.Close()
+	return nil
 }
 
 // Insert inserts the data in the database.
-func (t *timescaleDB) Insert(query string, data interface{}) error {
-	panic("Not implemented")
+func (t *timescaleDB) Insert(ctx context.Context, query string, arguments ...interface{}) error {
+	_, err := t.dbPool.Exec(ctx, query, arguments...)
+	if err != nil {
+		return err
+	}
+	return nil
 }
