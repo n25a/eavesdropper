28707e7cd85018a971240e24f51d90a4d7dc6506
Nima Mashhadi M. Reza <49960770+n25a@users.noreply.github.com>
2022-11-25 00:29:50 +0330

    🕛 Init timescale db (#6)
    * update db conf
    
    * add Insert function
    
    * add todo to readme
    
    * add schema path
    
    * add migration
    
    * update todo list
    
    * restruct interface
    
    * update db config
    
    * add dsn function

--- internal/database/database_conf.go
+++ internal/database/database_conf.go
@@ -1,3 +1,9 @@
 package database
 
-type Conf struct{}
+type Conf struct {
+	User         string `yaml:"user"`
+	Password     string `yaml:"password"`
+	Host         string `yaml:"host"`
+	Port         string `yaml:"port"`
+	DatabaseName string `yaml:"database_name"`
+}
--- internal/database/timescaledb.go
+++ internal/database/timescaledb.go
@@ -1,6 +1,22 @@
 package database
 
-type timescaleDB struct{}
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/golang-migrate/migrate"
+	libmigratepostgres "github.com/golang-migrate/migrate/database/postgres"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	"github.com/n25a/eavesdropper/internal/config"
+)
+
+type timescaleDB struct {
+	dbPool *pgxpool.Pool
+}
 
 // NewTimescaleDB returns a new instance of timescaleDB.
 func NewTimescaleDB() Database {
@@ -9,20 +25,66 @@ func NewTimescaleDB() Database {
 
 // Connect connects to the database.
 func (t *timescaleDB) Connect() error {
-	panic("Not implemented")
-}
-
-// CreateTable creates all tables in the database.
-func (t *timescaleDB) CreateTable() error {
-	panic("Not implemented")
+	var err error
+	ctx := context.Background()
+	t.dbPool, err = pgxpool.Connect(ctx, t.dsn())
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // Close closes the connection with the database.
 func (t *timescaleDB) Close() error {
-	panic("Not implemented")
+	t.dbPool.Close()
+	return nil
+}
+
+// Migrate creates all tables in the database.
+func (t *timescaleDB) Migrate() error {
+	// TODO: Create migration files from schema and migrate them.
+	if config.C.Database.MigrationPath == "" {
+		return errors.New("migration path is empty")
+	}
+
+	db, err := sql.Open("postgres", t.dsn())
+	if err != nil {
+		return err
+	}
+
+	driver, err := libmigratepostgres.WithInstance(db, &libmigratepostgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:///"+config.C.Database.MigrationPath,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+
+	return err
 }
 
 // Insert inserts the data in the database.
-func (t *timescaleDB) Insert(query string, data interface{}) error {
-	panic("Not implemented")
+func (t *timescaleDB) Insert(ctx context.Context, query string, arguments ...interface{}) error {
+	_, err := t.dbPool.Exec(ctx, query, arguments...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// dsn return data source name
+func (timescaleDB) dsn() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?parseTime=true&multiStatements=true&interpolateParams=true&collation=utf8mb4_general_ci",
+		config.C.Database.Conf.User, config.C.Database.Conf.Password, config.C.Database.Conf.Host,
+		config.C.Database.Conf.Port, config.C.Database.Conf.DatabaseName,
+	)
 }
